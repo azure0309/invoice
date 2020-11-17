26447c672a120fb46f059257d478238cf63766d4
Tuguldur <tuguldur@iMac.local>
2020-11-17 13:05:38 +0800

    commit 20201117

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -18,9 +18,36 @@ class InvoiceController extends Controller
     {
       $prev_month = date('Ym',strtotime("-2 month"));
       // return gettype(intval($prev_month));
-      $unpaid_numbers = Models\SimInvoice::where('BILL_MONTH', intval($prev_month))->
-      where('BILL_STATUS', 'UNPAID') ->get();
-      return view('invoice', ['unpaid_numbers'=>$unpaid_numbers]);
+      // Suuliin sariin hereglee hiisen dugaaruud
+      $last_month_numbers = Models\SimInvoice::where('BILL_MONTH', 202003)->
+      where('BILL_STATUS', 'UNPAID')->get();
+
+      // return view('invoice', ['last_month_numbers'=>$last_month_numbers]);
+      // echo $last_month_numbers;
+      foreach ($last_month_numbers as $number)
+      {
+        $getOperator = Models\SimInfo::where('PROD_NO', $number['PROD_NO'])->get('NAME');
+        foreach ($getOperator as $operator)
+        {
+          // echo $operator;
+          $getThreshold = Models\ThresholdModel::where('OPERATOR', $operator['NAME'])->get('THRESHOLD_TYPE');
+          foreach ($getThreshold as $threshold)
+          {
+            if ($threshold['THRESHOLD_TYPE'] == 'per SIM')
+            {
+              echo $number . 'per';
+            }
+            elseif ($threshold['THRESHOLD_TYPE'] == 'total SIM')
+            {
+              echo $number . 'total';
+            }
+          }
+        }
+        
+        // echo $getThreshold;
+        // return view('invoice', ['getOperator'->$getOperator]);
+      }
+      
       // echo $unpaid_numbers;
     }
 }
--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -3,23 +3,12 @@
 
 <table border=1>
   <tr>
-    <td>PROD_NO</td>
-    <td>BILL_MONTH</td>
-    <td>TOT_BILL_AMT</td>
-    <td>OVER_PYM</td>
-    <td>PYM_AMT</td>
-    <td>UPAID_AMT</td>
-    <td>BILL_STATUS</td>
+    <td>Operator</td>
   </tr>
-  @foreach($unpaid_numbers as $number)
+  @foreach($getOperator as $number)
   <tr>
-    <td>{{$number['PROD_NO']}}</td>
-    <td>{{$number['BILL_MONTH']}}</td>
-    <td>{{$number['TOT_BILL_AMT']}}</td>
-    <td>{{$number['OVER_PYM']}}</td>
-    <td>{{$number['PYM_AMT']}}</td>
-    <td>{{$number['UPAID_AMT']}}</td>
-    <td>{{$number['BILL_STATUS']}}</td>
+    <td>{{$number['NAME']}}</td>
   </tr>
   @endforeach
 </table>
+
